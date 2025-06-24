95405622ccc36aa77e49772d6569d80b87c09f77
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-06-24 20:36:28 +0545

    Favicon Added
    Favicon Added

---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Frello",
+    description: "Collaborate, Manage Projects and Reach new Productivity Peaks. From high rises to the Home Office, the way your team works is unique - accomplish it all with Frello.",
+};
