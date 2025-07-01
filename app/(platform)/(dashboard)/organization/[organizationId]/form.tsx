8bd77c4da7620865dabd1ab1d1a4c4f92faf4ec5
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-07-01 14:24:40 +0545

    Form Components - Input & Errors Added
    Form Components - Input & Errors Added

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import { create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
-import { useFormState } from "react-dom";
-import { FormInput } from "./form-input";
+import { createBoard } from "@/actions/create-board";
+
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
+import { FormInput } from "@/components/form/form-input";
 
 
 export const Form = () => {
-    const initialState = { message: "", errors: {} as { title?: string[] } };
-    const [state, dispatch] = useFormState(create, initialState);
+    const { execute, fieldErrors } = useAction(createBoard, {
+        onSuccess: (data) =>{
+            console.log(data, "SUCCESS!")
+        },
+        onError: (error) => {
+            console.error(error);
+        }
+    });
     
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+
+        execute({ title});
+
+    }
+
     return (
-        <form action={dispatch}>
+        <form action={onSubmit}>
             <div className="flex flex-col space-y-2">
-               <FormInput errors={state?.errors} />
+               <FormInput 
+                    label="Board Title"
+                    id="title"
+                    errors={fieldErrors as { title?: string[] }}
+               />
             </div>
             <FormButton />
         </form>
