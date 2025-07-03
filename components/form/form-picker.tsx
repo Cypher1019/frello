c93e66d4ad631dc4866ada09f2ce3c0d02d3d12e
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-07-03 08:07:35 +0545

    Form-Picker/Popover Updated
    Form-Picker/Popover Updated

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -2,10 +2,13 @@
 
 import { useEffect, useState } from "react";    
 import { unsplash } from "@/lib/unsplash";
-import { Loader2 } from "lucide-react";
+import { Check, Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { defaultImages } from "@/constants/images";
+import Link from "next/link";
+import { FormErrors } from "./form-errors";
 
 
 interface FormPickerProps {
@@ -19,7 +22,7 @@ export const FormPicker = ({
 }: FormPickerProps) => {
     const { pending } = useFormStatus();
 
-    const [images, setImages] = useState<Array<Record<string, any>>>([]);
+    const [images, setImages] = useState<Array<Record<string, any>>>(defaultImages);
     const [isLoading, setIsLoading] = useState(true);
     const [selectedImageId, setSelectedImageId] = useState(null);
 
@@ -41,7 +44,7 @@ export const FormPicker = ({
 
             } catch (error) {
                 console.log(error);
-                setImages([]);
+                setImages(defaultImages);
             }   finally {
                 setIsLoading(false);
             }
@@ -73,6 +76,15 @@ export const FormPicker = ({
                             setSelectedImageId(image.id);
                         }}
                     >
+                        <input 
+                            type="radio"
+                            id={id}
+                            name={id}
+                            className="hidden"
+                            checked={selectedImageId === image.id}
+                            disabled={pending}
+                            value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+                        />
                         <Image 
                             src ={image.urls.thumb}
                             alt = "Unsplash image"
@@ -80,10 +92,25 @@ export const FormPicker = ({
                             fill
                         
                         />
+                        {selectedImageId === image.id && (
+                            <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                                <Check className="h-4 w-4 text-white " />
+                            </div>
+                        )}
+                        <Link 
+                            href={image.links.html}
+                            target="_blank"
+                            className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover:underline p-1 bg-black/50"
+                        >
+                            {image.user.name}
+                        </Link>
                     </div>
                 ))}
             </div>
-            
+            <FormErrors 
+                id ="image"
+                errors={errors}
+            />
         </div>
     );
 };
