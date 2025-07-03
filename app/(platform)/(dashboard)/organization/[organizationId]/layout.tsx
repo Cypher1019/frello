2c8688770d3ae89667f08b0337e1414cb72d8e0e
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-07-03 14:20:18 +0545

    Board Components
    Board Components

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,16 @@
+import { startCase } from "lodash";
+
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs/server";
+import { title } from "process";
+
+export async function generateMetadata() {
+    const { orgSlug } = await auth();
+
+    return{
+        title: startCase(orgSlug || "organization"),
+    };
+};
 
 const OrganizationIdLayout = ({
     children
