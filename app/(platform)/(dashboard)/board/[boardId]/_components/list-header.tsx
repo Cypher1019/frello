2ebe0411e0210a61613ab3013ba346470a57110b
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-07-07 11:26:40 +0545

    List Options Added
    List Options Added

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -8,6 +8,7 @@ import { FormInput } from "@/components/form/form-input";
 import { useAction } from "@/hooks/use-action";
 import { updateList } from "@/actions/update-list";
 import { toast } from "sonner";
+import { ListOptions } from "./list-options";
 
 interface ListHeaderProps {
     data: List;
@@ -104,7 +105,10 @@ export const ListHeader = ({
                 {data.title}
             </div>
             )}
-            
+            <ListOptions 
+                onAddCard = {() => {}}
+                data={data}
+            />
         </div>
     );
 };
