12f679457bd58eddbe9b8e5d7b055c1ff2427f4d
Sauharda Shrestha <111726300+Cypher1019@users.noreply.github.com>
2025-07-08 10:28:34 +0545

    List Copy Added
    List Copy Added

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -15,6 +15,8 @@ import { Separator } from "@/components/ui/separator";
 import { useAction } from "@/hooks/use-action";
 import { deleteList } from "@/actions/delete-list";
 import { toast } from "sonner";
+import { ElementRef, useRef } from "react";
+import { copyList } from "@/actions/copy-list";
 
 
 interface ListOptionsProps {
@@ -26,9 +28,22 @@ export const ListOptions = ({
     data,
     onAddCard,
 }: ListOptionsProps) => {
+    const closeRef = useRef<ElementRef<"button">>(null);
+
     const { execute: executeDelete } = useAction(deleteList, {
         onSuccess: (data) => {
             toast.success(`List "${data.title}" deleted`);
+            closeRef.current?.click();
+        },
+        onError: (error) => {
+            toast.error(error);
+        }
+    });
+
+     const { execute: executeCopy } = useAction(copyList, {
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" copied`);
+            closeRef.current?.click();
         },
         onError: (error) => {
             toast.error(error);
@@ -40,6 +55,13 @@ export const ListOptions = ({
         const boardId = formData.get("boardId") as string;
 
         executeDelete({id, boardId});
+    };
+
+    const onCopy = (formData: FormData) => {
+        const id = formData.get("id") as string;
+        const boardId = formData.get("boardId") as string;
+
+        executeCopy({id, boardId});
     };
 
 
@@ -55,7 +77,7 @@ export const ListOptions = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     List Actions
                 </div>
-                <PopoverClose asChild>
+                <PopoverClose ref={closeRef} asChild>
                     <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost">
                         <X className="h-4 w-4" />
                     </Button>
@@ -66,7 +88,7 @@ export const ListOptions = ({
                 >
                     Add card....
                 </Button>
-                <form>
+                <form action={onCopy}>
                     <input hidden name="id" id="id" value={data.id} />
                     <input hidden name="boardId" id="boardId" value={data.boardId} />
                     <FormSubmit
